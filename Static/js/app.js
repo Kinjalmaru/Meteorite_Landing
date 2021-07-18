c292fdc20b52b0c1136eec98ae2eee7d8edc8c01
cmoore187 <calebmoore187@gmail.com>
2021-07-18 14:50:04 -0400

    Updated style.

--- Static/js/app.js
+++ Static/js/app.js
@@ -27,50 +27,48 @@ var chartGroup = svg.append("g")
 // Load data from hours-of-tv-watched.csv
 d3.csv("Data/impacts.csv").then(function(Data) {
 
-  console.log(Data);
+    // Cast the impacts value to a number for each piece of Data
+    Data.forEach(function(d) {
+      d.impacts = +d.impacts;
+    });
 
-  // Cast the hours value to a number for each piece of tvData
-  Data.forEach(function(d) {
-    d.impacts = +d.impacts;
-  });
+    // Configure a band scale for the horizontal axis with a padding of 0.1 (10%)
+    var xBandScale = d3.scaleBand()
+      .domain(Data.map(d => d.name))
+      .range([0, chartWidth])
+      .padding(0.1);
 
-  // Configure a band scale for the horizontal axis with a padding of 0.1 (10%)
-  var xBandScale = d3.scaleBand()
-    .domain(Data.map(d => d.name))
-    .range([0, chartWidth])
-    .padding(0.1);
+    // Create a linear scale for the vertical axis.
+    var yLinearScale = d3.scaleLinear()
+      .domain([0, d3.max(Data, d => d.impacts)])
+      .range([chartHeight, 0]);
 
-  // Create a linear scale for the vertical axis.
-  var yLinearScale = d3.scaleLinear()
-    .domain([0, d3.max(Data, d => d.impacts)])
-    .range([chartHeight, 0]);
+    // Create two new functions passing our scales in as arguments
+    // These will be used to create the chart's axes
+    var bottomAxis = d3.axisBottom(xBandScale);
+    var leftAxis = d3.axisLeft(yLinearScale).ticks(10);
 
-  // Create two new functions passing our scales in as arguments
-  // These will be used to create the chart's axes
-  var bottomAxis = d3.axisBottom(xBandScale);
-  var leftAxis = d3.axisLeft(yLinearScale).ticks(10);
+    // Append two SVG group elements to the chartGroup area,
+    // and create the bottom and left axes inside of them
+    chartGroup.append("g")
+      .call(leftAxis);
 
-  // Append two SVG group elements to the chartGroup area,
-  // and create the bottom and left axes inside of them
-  chartGroup.append("g")
-    .call(leftAxis);
+    chartGroup.append("g")
+      .attr("transform", `translate(0, ${chartHeight})`)
+      .call(bottomAxis);
 
-  chartGroup.append("g")
-    .attr("transform", `translate(0, ${chartHeight})`)
-    .call(bottomAxis);
-
-  // Create one SVG rectangle per piece of tvData
-  // Use the linear and band scales to position each rectangle within the chart
-  chartGroup.selectAll(".bar")
-    .data(Data)
-    .enter()
-    .append("rect")
-    .attr("class", "bar")
-    .attr("x", d => xBandScale(d.name))
-    .attr("y", d => yLinearScale(d.impacts))
-    .attr("width", xBandScale.bandwidth())
-    .attr("height", d => chartHeight - yLinearScale(d.impacts));
-
+    // Create one SVG rectangle per piece of Data
+    // Use the linear and band scales to position each rectangle within the chart
+    chartGroup.selectAll(".bar")
+      .data(Data)
+      .enter()
+      .append("rect")
+      .attr("class", "bar")
+      .attr("x", d => xBandScale(d.name))
+      .attr("y", d => yLinearScale(d.impacts))
+      .attr("width", xBandScale.bandwidth())
+      .attr("height", d => chartHeight - yLinearScale(d.impacts));
+ 
 }).catch(function(error) {
   console.log(error);
 });
